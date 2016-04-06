17e15b6b459723a8251f1a3671abc2543cf0fb1f
Manuel <manuelroesch@gmail.com>
2016-04-06 14:45:21 +0200

    asdf

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,8 +3,10 @@
 # --- !Ups
 
 create table "USERS" ("NAME" VARCHAR(254) NOT NULL,"ID" SERIAL NOT NULL PRIMARY KEY);
+create table "log" ("id" INTEGER NOT NULL PRIMARY KEY, "accesstime" TIMESTAMP NOT NULL, "url" VARCHAR(1024) NOT NULL, "ip" VARCHAR(254) NOT NULL, "USER" INTEGER);
 
 # --- !Downs
 
 drop table "USERS";
+drop table "log";
 
