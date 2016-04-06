a58f571be4c92c7f5d89b6a687e3895479ec9212
Manuel <manuelroesch@gmail.com>
2016-04-06 14:21:11 +0200

    asdf

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,14 +1,14 @@
-# Log schemma
+--- Log schemma
 
-# --- !Ups
+--- !Ups
 
 CREATE TABLE log (
-  id int(11) UNSIGNED NOT NULL,
-  accesstime datetime NOT NULL,
+  id INTEGER NOT NULL,
+  accesstime TIMESTAMP  NOT NULL,
   url varchar(1024) NOT NULL DEFAULT '',
   ip varchar(254) NOT NULL DEFAULT '',
-  USER int(11) DEFAULT NULL
+  USER INTEGER DEFAULT NULL
 )
 
 
-# --- !Downs
+--- !Downs
