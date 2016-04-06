5c7179626518372e113bc1039a616736858367fd
Manuel <manuelroesch@gmail.com>
2016-04-06 14:49:37 +0200

    asdf

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Log.createEntry("test", "test2", 123)
+    //(Log.createEntry("test", "test2", 123)
     Ok(views.html.index("Your new application is ready."))
   }
 
