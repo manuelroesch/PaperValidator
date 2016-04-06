ea52026e8578fb5a43231c78aab0f422b1cb2378
Manuel <manuelroesch@gmail.com>
2016-04-06 12:12:25 +0200

    asdf

---
+++ app/models/Log.scala
@@ -0,0 +1,31 @@
+package models
+
+import org.joda.time.DateTime
+import play.api.Play.current
+import play.api.db.DB
+import scalikejdbc.SQL
+
+/**
+  * Created by pdeboer on 20/11/15.
+  */
+object Log {
+	def createEntry(url: String, ip: String, userId: Long): Unit = {
+		DB.withConnection { implicit c =>
+			SQL("INSERT INTO log (accesstime, url, ip, user) VALUES (NOW(), {url}, {ip}, {userId})").on(
+				'url -> url,
+				'ip -> ip,
+				'userId -> userId
+			).executeInsert()
+		}
+	}
+
+	def ipLogEntriesSince(ip: String, since: DateTime): Long = {
+		val head = DB.withConnection { implicit c =>
+			SQL("SELECT count(*) AS cnt FROM log WHERE accesstime >= {accesstime} AND ip = {ip}").on(
+				'accesstime -> since,
+				'ip -> ip
+			).apply().head
+		}
+		head[Long]("cnt")
+	}
+}
