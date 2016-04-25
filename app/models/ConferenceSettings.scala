fc184caf8869f03f0719d872a88382537deb6bb7
Manuel <manuelroesch@gmail.com>
2016-04-25 12:28:46 +0200

    asdf

--- app/models/ConferenceSettings.scala
+++ app/models/ConferenceSettings.scala
@@ -39,7 +39,7 @@ class ConferenceSettingsService @Inject()(db:Database) {
 
 	def findAllByConference(conferenceId : Int): List[ConferenceSettings] = {
 		db.withConnection { implicit c =>
-			SQL("SELECT cs.id, m2a.id method2assumption_id, m2a.method_name, m2a.assumption_name, cs.flag " +
+			SQL("SELECT cs.id, m2a.m2aid method2assumption_id, m2a.method_name, m2a.assumption_name, cs.flag " +
 						"FROM (SELECT m2a.id m2aid, m2a.method_id, m.name method_name, m2a.assumption_id, a.name assumption_name " +
 						"FROM methods2assumptions m2a,methods m,assumptions a " +
 						"WHERE m2a.method_id=m.id AND m2a.assumption_id=a.id) m2a " +
