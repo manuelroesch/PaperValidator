33576d8cb3bd13c3fbdfeb6d162487d18b8d90e4
Manuel <manuelroesch@gmail.com>
2016-04-26 12:43:59 +0200

    asdf

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -75,7 +75,8 @@ class Application @Inject() (configuration: Configuration, questionService: Ques
 
 		} else {
 			assert(!workerId.isEmpty)
-			val newSession = request.session + ("TurkerID" -> workerId) + ("assignmentId" -> assignmentId) + ("target" -> target)
+			//val newSession = request.session + ("TurkerID" -> workerId) + ("assignmentId" -> assignmentId) + ("target" -> target)
+			val newSession = request.session + (Application.TURKER_ID_KEY -> workerId) + ("assignmentId" -> assignmentId) + ("target" -> target)
 
 			showQuestionAction(uuid, secret, request, Some(workerId), Some(newSession))
 		}
