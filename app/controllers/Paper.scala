4155cecaa062e573c050d266a10ae967af32e490
Manuel <manuelroesch@gmail.com>
2016-06-17 02:12:33 +0200

    Add Answer Evaluation Table to Show Paper Processing Log

--- app/controllers/Paper.scala
+++ app/controllers/Paper.scala
@@ -2,9 +2,11 @@ package controllers
 
 import javax.inject.Inject
 
+import ch.uzh.ifi.pdeboer.pplib.hcomp.ballot.integrationtest.console.Constants
+import play.libs.Json
+
 import scala.collection.mutable.ListBuffer
 import scala.reflect.io.File
-import scala.util.matching.Regex
 import util.control.Breaks._
 import helper.{SpellChecker, Commons, PaperProcessingManager}
 import models._
@@ -25,10 +27,10 @@ class Paper @Inject()(database: Database, configuration: Configuration, papersSe
 
   def show(id:Int, secret:String) = Action {
     val paper = papersService.findByIdAndSecret(id,secret)
-    if(paper.size > 0) {
+    if(paper.isDefined) {
       var results = paperResultService.findByPaperId(id)
       results = addMethodsAndAssumptions(id,results)
-      val spellCheck = SpellChecker.check(paper.get)
+      val spellCheck = ""//SpellChecker.check(paper.get)
       val filePath = configuration.getString("highlighter.pdfSourceDir").get+"/"+Commons.getSecretHash(secret)+"/log.txt"
       val fileLengh = File(filePath).length
       var log = ""
@@ -36,26 +38,47 @@ class Paper @Inject()(database: Database, configuration: Configuration, papersSe
         val source = scala.io.Source.fromFile(filePath)
         log = try source.mkString.replace("\n","\n<br>") finally source.close()
       }
-      var snippetFilename = ""
-      val answers = answerService.findJsonAnswerByPaperId(id).map(a => {
-        val parsedJSON = a.answerJson.replace("{\"","").replace("\"}","").replace("\" : \"",": ").replace("\", \"","<br>\n")
-        if(snippetFilename != a.snippetFilename && !a.answerJson.isEmpty) {
-          snippetFilename = a.snippetFilename
-            val imgPath = a.snippetFilename.substring(a.snippetFilename.indexOf("tmp")+4)
-            val imgUrl = configuration.getString("url.prefix").get + routes.Paper.getFile("tmp",imgPath).url
-            val img = "<hr><a href='" + imgUrl + "'><img class='img-responsive' src='" + imgUrl + "'></a><br>"
-              "<br><br>" + img + "<br>" + parsedJSON
-        } else {
-          "<br><br>" + parsedJSON
-        }
-      }).mkString("")
-      Ok(views.html.paper.showPaper(paper.get,Commons.getSecretHash(secret),results,spellCheck,log,answers))
+      val answers = answerService.findJsonAnswerByPaperId(id)
+      val answersEvaluated = evaluateAnswers(answers)
+      Ok(views.html.paper.showPaper(paper.get,Commons.getSecretHash(secret),results,spellCheck,log,answers,answersEvaluated))
     } else {
       Unauthorized(views.html.error.unauthorized())
     }
   }
 
-  def addMethodsAndAssumptions(id:Int,results: List[PaperResult]) : List[PaperResult] = {
+  def evaluateAnswers(answers: List[AnswerShowPaper]): Map[String,Int] = {
+    var countAnswers: Map[String,Int] = Map()
+    var confidenceList : ListBuffer[Int] = ListBuffer()
+    var snippetFilename = ""
+    answers.foreach(a => {
+      if(!a.answerJson.isEmpty) {
+        val parsedJSON = Json.parse(a.answerJson)
+        if (snippetFilename != a.snippetFilename || a == answers.last) {
+          if(confidenceList.nonEmpty) {
+            val confMean = confidenceList.sum / confidenceList.length.toFloat
+            val confVar = confidenceList.map(cv => Math.pow(cv-confMean,2)).sum / confidenceList.length
+            countAnswers += ((snippetFilename+"confMean") -> Math.round(confMean*10))
+            countAnswers += ((snippetFilename+"confVar") -> Math.round(confVar.toFloat*10))
+            confidenceList = ListBuffer()
+          }
+          snippetFilename = a.snippetFilename
+        }
+          val confidence = parsedJSON.get("confidence").asInt()
+          List("isRelated","isCheckedBefore").foreach(a => {
+            val countKey = snippetFilename+a+parsedJSON.get(a).asText()+"-"+(confidence>=Constants.LIKERT_VALUE_CLEANED_ANSWERS)
+            if(countAnswers.isDefinedAt(countKey)) {
+              countAnswers += (countKey -> (countAnswers(countKey)+1))
+            } else {
+              countAnswers += (countKey -> 1)
+            }
+          })
+          confidenceList += confidence
+      }
+    })
+    countAnswers
+  }
+
+  def addMethodsAndAssumptions(id:Int, results: List[PaperResult]) : List[PaperResult] = {
     var allResults = results
     val paper = papersService.findById(id).get
     val m2aList = answerService.findByPaperId(paper.id.get)
@@ -96,7 +119,7 @@ class Paper @Inject()(database: Database, configuration: Configuration, papersSe
         if(confSetting.flag.get==ConferenceSettings.FLAG_EXPECT) {
           symbol = PaperResult.SYMBOL_WARNING
         }
-        allResults = allResults:+ new PaperResult(Some(1L),id,PaperResult.TYPE_M2A,m2aDescr,m2aResult,symbol)
+        allResults:+ new PaperResult(Some(1L),id,PaperResult.TYPE_M2A,m2aDescr,m2aResult,symbol)
       }
     })
     allResults
@@ -104,15 +127,12 @@ class Paper @Inject()(database: Database, configuration: Configuration, papersSe
 
   def getM2AFlagText(confSetting: ConferenceSettings): String = {
     confSetting.flag.get match {
-      case ConferenceSettings.FLAG_REQUIRE => {
+      case ConferenceSettings.FLAG_REQUIRE =>
         "<span class='m2aFlag text-danger glyphicon glyphicon-flag'><span>"
-      }
-      case ConferenceSettings.FLAG_EXPECT => {
+      case ConferenceSettings.FLAG_EXPECT =>
         "<span class='m2aFlag text-warning glyphicon glyphicon-flag'><span>"
-      }
-      case ConferenceSettings.FLAG_IGNORE => {
+      case ConferenceSettings.FLAG_IGNORE =>
         "<span class='m2aFlag text-muted glyphicon glyphicon-flag'><span>"
-      }
     }
   }
 
